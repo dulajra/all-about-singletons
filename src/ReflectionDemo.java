ca2da38ead595cb2e04c24af1454ec8b896bfa57
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-07-04 01:57:45 +0530

    Demo of reflection problem with singletons

---
+++ src/ReflectionDemo.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Constructor;
+
+/**
+ * @author Dulaj Atapattu
+ */
+public class ReflectionDemo {
+
+    public static void main(String[] args) throws Exception {
+        Singleton singleton = Singleton.INSTANCE;
+
+        Constructor constructor = singleton.getClass().getDeclaredConstructor(new Class[0]);
+        constructor.setAccessible(true);
+
+        Singleton singleton2 = (Singleton) constructor.newInstance();
+
+        if (singleton == singleton2) {
+            System.out.println("Two objects are same");
+        } else {
+            System.out.println("Two objects are not same");
+        }
+
+        singleton.setValue(1);
+        singleton2.setValue(2);
+
+        System.out.println(singleton.getValue());
+        System.out.println(singleton2.getValue());
+
+    }
+}
