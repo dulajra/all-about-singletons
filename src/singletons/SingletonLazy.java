2161ea5c0305ef92d7b2a92d551a3b299621d243
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-07-04 01:56:26 +0530

    Singleton with lazy initialization and fine grained synchronization

---
+++ src/singletons/SingletonLazy.java
@@ -0,0 +1,24 @@
+package singletons;
+
+/**
+ * @author Dulaj Atapattu
+ */
+public class SingletonLazy {
+
+    private static SingletonLazy INSTANCE = null;
+
+    private SingletonLazy() {
+    }
+
+    public static SingletonLazy getInstance() {
+        if (INSTANCE == null) {
+            synchronized (SingletonLazy.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new SingletonLazy();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
